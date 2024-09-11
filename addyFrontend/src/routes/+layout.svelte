05e7d32f31dc1d50b5625c09165306e903e0398e
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-09-11 12:36:58 +0530

    footer component added

--- addyFrontend/src/routes/+layout.svelte
+++ addyFrontend/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
   import "../app.css";
+  import Footer from "../lib/Footer.svelte";
   import Header from "../lib/Header.svelte";
   import MobileHeader from "../lib/MobileHeader.svelte";
 </script>
 
 <Header />
 <MobileHeader />
+
 <slot />
+<Footer />
