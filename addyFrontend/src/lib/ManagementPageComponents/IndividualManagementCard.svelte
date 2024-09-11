aa61c01ee61f120d713981330eb1fbaab091b251
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-09-11 12:40:02 +0530

    management page added

---
+++ addyFrontend/src/lib/ManagementPageComponents/IndividualManagementCard.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { gsToHttp } from "$lib/CommonComponents/utils.js";
+
+  export let individualCardData;
+
+  console.log();
+</script>
+
+<div
+  class="border border-black rounded-xl p-2 flex flex-col gap-5 shadow shadow-[#5e17eb]"
+>
+  <div class="">
+    <img
+      src={gsToHttp(individualCardData.image)}
+      alt=""
+      class="w-full h-96 object-cover rounded-xl"
+    />
+  </div>
+  <div class=" text-center">
+    <p class="text-2xl font-bold text-[#5e17eb] raleway-font">
+      {individualCardData.name}
+    </p>
+    <!-- <p class="raleway-font">With {individualCardData.coach}</p> -->
+
+    <p class="mt-2 montserrat-regular">
+      {individualCardData.description}
+    </p>
+  </div>
+  <button
+    class="py-2 rounded-xl border border-black bg-black text-white hover:bg-white hover:text-black"
+    >Contact Us for More Information</button
+  >
+</div>
+
+<style>
+  .raleway-font {
+    font-family: "Raleway", sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+  }
+
+  .montserrat-regular {
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+  }
+</style>
---
+++ addyFrontend/src/routes/management/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import IndividualHealthcareCard from "../../lib/HealthcarePageComponents/IndividualHealthcareCard.svelte";
+  import IndividualManagementCard from "../../lib/ManagementPageComponents/IndividualManagementCard.svelte";
+  import IndividualNutritionCard from "../../lib/NutritionPageComponents/IndividualNutritionCard.svelte";
+
+  let nutritionCardData = [
+    {
+      id: 1,
+      name: "Naming Partner",
+      description: "We Help You Grow",
+      image: "gs://addyfitness-db121.appspot.com/namingPartner.gif",
+    },
+    {
+      id: 2,
+      name: "Managing Partner",
+      description: "Let's Grow Together",
+      image: "gs://addyfitness-db121.appspot.com/managingPartner.gif",
+    },
+    {
+      id: 3,
+      name: "Franchise",
+      description: "let's Make a Community Together",
+      image: "gs://addyfitness-db121.appspot.com/franchise.gif",
+    },
+  ];
+</script>
+
+<div class="md:p-10 p-5 grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-4">
+  {#each nutritionCardData as individualData (individualData.id)}
+    <IndividualManagementCard individualCardData={individualData} />
+  {/each}
+</div>
