50dc1adb8240190b91d572ab0651639c71575b94
Ayush Jhunjhunwala <48875674+the-non-expert@users.noreply.github.com>
2025-01-24 20:37:03 +0530

    healthcare card design updated

--- addyFrontend/src/routes/healthcare/+page.svelte
+++ addyFrontend/src/routes/healthcare/+page.svelte
@@ -47,7 +47,7 @@
   <meta name="description" content="Join the AddyFitness team and make a difference in people's lives through fitness and wellness." />
 </svelte:head>
 
-<div class="md:p-10 p-5 grid grid-cols-1 md:grid-cols-4 gap-x-10 gap-y-4">
+<div class="md:p-10 p-5 grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-4">
   {#each nutritionCardData as individualData (individualData.id)}
     <IndividualHealthcareCard individualCardData={individualData} />
   {/each}
